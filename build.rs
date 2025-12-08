e8c44550f3096431aee11edfde665e3914ad3176
gLofs <jeanyida@gmail.com>
2025-12-09 00:10:29 +0800

    feat(forum):add proto restful

--- build.rs
+++ build.rs
@@ -1,11 +1,20 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use std::io::Result;
+
+fn main() -> Result<()> {
+    // 1. 设置 PROTOC 环境变量
     unsafe {
         std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
     }
-    tonic_build::configure()
-        .compile_protos(
-            &["proto/forum.proto"], 
-            &["proto"],
-        )?;
+
+    let mut config = prost_build::Config::new();
+
+    // 2. 添加 Serde 序列化支持
+    config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+
+    // 3. 编译 Proto 文件
+    config.compile_protos(
+        &["proto/forum.proto"],
+        &["proto"])?;
+
     Ok(())
 }
